75d58de27cb32c18a2832407e4c525840084f824
Viktor Lazar <vlazar@outlook.com>
2018-02-11 14:18:33 +0100

    fixed problem with library dependecies

--- src/WifiLocation.h
+++ src/WifiLocation.h
@@ -3,11 +3,7 @@
 #ifndef _WIFILOCATION_h
 #define _WIFILOCATION_h
 
-#if defined(ARDUINO) && ARDUINO >= 100
 #include "Arduino.h"
-#else
-#include "WProgram.h"
-#endif
 
 //#define DEBUG_WIFI_LOCATION
 
